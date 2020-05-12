3b26d1977c67191df6878080aa5c8528544f5acc
Brett <b.younginger@gmail.com>
2020-05-11 17:38:22 -0700

    String updates

--- W3R/w3r_lists.py
+++ W3R/w3r_lists.py
@@ -31,4 +31,41 @@ def multer(lst):
 
 multer(sample)
 
+'''3. Write a Python program to get the largest number from a list.'''
+
+max(sample)
+
+'''4. Write a Python program to get the smallest number from a list.'''
+
+min(sample)        
+
+'''
+5. Write a Python program to count the number of strings where the 
+string length is 2 or more and the first and last character are same 
+from a given list of strings.
+Sample List : ['abc', 'xyz', 'aba', '1221']
+Expected Result : 2'''
+
+def weird(lststr):
+    
+    empty_count = 0
+    
+    for i in lststr:
         
+        if len(i) > 2:
+            
+            if i[0] == i[-1]:
+                
+              empty_count += 1
+    
+    return empty_count
+
+sample_list = ['abc', 'xyz', 'aba', '1221', 'aa', '1222221']
+
+weird(sample_list)
+
+'''6. Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples. 
+Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]'''
+
+
